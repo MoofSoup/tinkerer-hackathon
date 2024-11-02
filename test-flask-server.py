95593da953f586676642a79e2033ee1ec5691cb7
Moof Soup <moofsoup@gmail.com>
2024-11-02 16:49:26 -0700

    updated the test to look on port 8080

--- test-flask-server.py
+++ test-flask-server.py
@@ -4,15 +4,15 @@ import unittest
 
 class TestFlaskServer(unittest.TestCase):
     def setUp(self):
-        self.base_url="http://0.0.0.0:5432"
+        self.base_url="http://0.0.0.0:8080"
         self.endpoint="/is_poisoned"
 
     def testServerRunning(self):
         try:
-            response = requests.get(f"{self.base_url}{self.endpoint}")
+            response = requests.post(f"{self.base_url}{self.endpoint}")
             self.assertEqual(response.status_code, 200)
         except:
-            self.fail("server is not running at 0.0.0.0:5432")
+            self.fail("server is not running at 0.0.0.0:8080")
 
 if __name__ == '__main__':
     unittest.main()
