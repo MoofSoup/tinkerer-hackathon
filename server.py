69fae7f485ce3045590a0952e0589e1e679a5d4b
Moof Soup <moofsoup@gmail.com>
2024-11-02 16:14:30 -0700

    initial commit of server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/is_poisoned', methods=['POST'])
+def is_poisoned():
+    return jsonify({"is_poisoned": True})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
