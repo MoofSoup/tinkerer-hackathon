14f7345072d22097df08865af8aa0eeff387839e
Moof Soup <moofsoup@gmail.com>
2024-11-02 16:00:12 -0700

    updated flask server test

--- test-flask-server.py
+++ test-flask-server.py
@@ -1,1 +1,18 @@
 # this script tests if a flask server is running locally
+import requests
+import unittest
+
+class TestFlaskServer(unittest.TestCase):
+    def setUp(self):
+        self.base_url="http://0.0.0.0:5432"
+        self.endpoint="/is_poisoned"
+
+    def testServerRunning(self):
+        try:
+            response = requests.get(f"{self.base_url}{self.endpoint}")
+            self.assertEqual(response.status_code, 200)
+        except:
+            self.fail("server is not running at 0.0.0.0:5432")
+
+if __name__ == '__main__':
+    unittest.main()
