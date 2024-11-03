eea46755fd035017d951542132c6371b19272eeb
Moof Soup <moofsoup@gmail.com>
2024-11-02 18:40:53 -0700

    updates

--- check_if_poisoned.py
+++ check_if_poisoned.py
@@ -6,6 +6,7 @@ def hello_world(str) -> str:
     return "hello world!"
 
 def hello_openai(prompt:str) -> str:
+    load_dotenv('.env')
     client = OpenAI()
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -14,7 +15,8 @@ def hello_openai(prompt:str) -> str:
             {"role": "user", "content": prompt}
         ],
     )
-    return completion.choices[0].message
+    return completion.choices[0].message.content
 
 if __name__ == '__main__':
-    hello_openai("the game is about chickens, what should I do?")
+    val = hello_openai("the game is about chickens, what should I do?")
+    print(val)
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, request
+from check_if_poisoned import hello_world, hello_openai
 
 app = Flask(__name__)
 
 @app.route('/is_poisoned', methods=['POST'])
 def is_poisoned():
-    return jsonify({"is_poisoned": True})
+    data = request.get_json()
+    text = data.get('text', '')
+    val = hello_openai(text)
+    return jsonify({"is_poisoned": val})
 
 
 if __name__ == '__main__':
